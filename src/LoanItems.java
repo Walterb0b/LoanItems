4bc929dc6220acdf7059f65072fb48e3c51527e2
Walterb0b <osca0001@stud.ek.dk>
2025-09-17 09:06:08 +0200

    loan items færdig

--- src/LoanItems.java
+++ src/LoanItems.java
@@ -1,2 +1,29 @@
 public class LoanItems {
+    //Attributes til vores loan items
+    private String title;
+    private String type;
+
+    //Constructor til vores loan items
+    LoanItems(String title, String type){
+        this.title = title;
+        this.type = type;
+    }
+
+    //Getters
+    public String getTitle() {return title;}
+    public String getType() {return type;}
+
+    //Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    @Override
+    public String toString(){
+        return getTitle() + " " + "(" + getType() + ")";
+    }
 }
