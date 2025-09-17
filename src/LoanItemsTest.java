7fd8e99751805936500f609ea7b5485f9320e788
lukasma02 <andersenlukas02@gmail.com>
2025-09-17 09:20:13 +0200

    new

--- src/LoanItemsTest.java
+++ src/LoanItemsTest.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class LoanItemsTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What would you like register today?");
+
+        String[] Items = {};
+        int numberOfItems = sc.nextInt();
+        sc.nextline();
+
+    }
+
 }
