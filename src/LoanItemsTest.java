74eedd4e68e936d7295e69e32f2b10bb9c6bf8a7
Walterb0b <osca0001@stud.ek.dk>
2025-09-17 09:39:31 +0200

    check int

--- src/LoanItemsTest.java
+++ src/LoanItemsTest.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class LoanItemsTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("What would you like register today?");
-        int numberOfItems = sc.nextInt();
-        sc.nextline();
+        int numberOfItems;
+        do{
+            System.out.print("How many items would you like to register: ");
+        while(!sc.hasNextInt()){
+                System.out.print("Please enter a positive number: ");
+                sc.next();
+            }
+            numberOfItems = sc.nextInt();
+        }while(numberOfItems <= 0);{
+                System.out.println("Noted that you would like to register " + numberOfItems + " items");
+        }
+
+
 
         String[] Items = {};
 
