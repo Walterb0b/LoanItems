6ebbf1059eb567d94c2fbb767be99931c0a40137
Walterb0b <osca0001@stud.ek.dk>
2025-09-17 09:23:33 +0200

    lol

--- src/LoanItemsTest.java
+++ src/LoanItemsTest.java
@@ -4,10 +4,11 @@ public class LoanItemsTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("What would you like register today?");
-
-        String[] Items = {};
         int numberOfItems = sc.nextInt();
         sc.nextline();
+
+        String[] Items = {};
+
 
     }
 
