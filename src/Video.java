97084aadb92906d4942f440f314445b2a703fca4
lukasma02 <andersenlukas02@gmail.com>
2025-09-17 11:48:42 +0200

    med kommentarer

--- src/Video.java
+++ src/Video.java
@@ -1,18 +1,18 @@
 public class Video extends LoanItems{
 
-    private int length;
+    private int length; //Attribute til at bestemme længde
 
     Video(String title, String type, int length){
         super (title, type);
-        this.length = length;
+        this.length = length; // Konstruktør til at definere længde
     }
 
-    //Metode til at returnere længden af vores video
+    //Metode til at returnere længden af video i minutter
     public int getLength() {return length;}
 
     @Override
     public String toString() {
-        return super.toString()+ "\t" + getLength();
+        return super.toString()+ "\t" + getLength(); // Returnerer længde
 
     }
 }
